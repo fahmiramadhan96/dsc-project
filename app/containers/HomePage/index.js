04d72f71826428cb142ddd95c541b442dff9824d
Fahmi Ramadhan <fahmi.ramadhan@photoninfotech.net>
2020-09-04 05:59:33 +0700

    test commit

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,6 +12,7 @@ import messages from './messages';
 export default function HomePage() {
   return (
     <h1>
+      <div>INI TEST KOCIK</div>
       <FormattedMessage {...messages.header} />
     </h1>
   );
